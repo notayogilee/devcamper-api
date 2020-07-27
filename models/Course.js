86dc6ad0f5513033851c4dddee5e3e4214138c50
Lee Castelani <notayogilee@gmail.com>
2020-07-27 08:54:11 -0400

    upload photo for bootcamp with own file and id based on bootcamp id

--- models/Course.js
+++ models/Course.js
@@ -40,7 +40,6 @@ const CourseSchema = new mongoose.Schema({
 
 // Static method to get average of course tuitions
 CourseSchema.statics.getAverageCost = async function (bootcampId) {
-  console.log('Calculating avg cost...'.blue);
 
   const obj = await this.aggregate([
     {
