93e71a8738a036cf0838cb4acba44c0eadcbbc7b
Lee Castelani <notayogilee@gmail.com>
2020-07-27 16:43:10 -0400

    add password hashing and user token

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,52 @@ const Bootcamp = require('../models/Bootcamp');
 const User = require('../models/User');
 
 // @desc - Register User
-// @route - GET /api/v1/auth/register
+// @route - POST /api/v1/auth/register
 // @access - public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc - Login User
+// @route - POST /api/v1/auth/login
+// @access - public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide and email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches 
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
