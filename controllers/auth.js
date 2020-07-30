28824119da668fb1a4dfdad31ac0c5f1b32775e5
Lee Castelani <notayogilee@gmail.com>
2020-07-30 09:36:56 -0400

    add nodemailer and mailtrap to send and retrap reset password message -supercool!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
-const Bootcamp = require('../models/Bootcamp');
+// const Bootcamp = require('../models/Bootcamp');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc - Register User
@@ -75,12 +76,38 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  console.log(resetToken);
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
 
-  res.status(200).json({
-    success: true,
-    data: user
-  })
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to /n/n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent' });
+  } catch (err) {
+    console.error(err);
+
+    user.getResetPasswordToken = undefined;
+    user.getResetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
+
+  // console.log(resetToken);
+
+  // This was causing error 'cannot send headers after they are sent to client
+
+  // res.status(200).json({
+  //   success: true,
+  //   data: user
+  // })
 });
 
 // Get token from model, also create cookie and send response
