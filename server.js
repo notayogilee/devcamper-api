bd8c0dfab12f09bf30fcbf9422772b97c848321d
Lee Castelani <notayogilee@gmail.com>
2020-08-01 09:24:21 -0400

    add more security to API

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -40,6 +45,26 @@ app.use(fileupload());
 
 // Sanitize data 
 app.use(mongoSanitize());
+
+// Set security headers 
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windomMs: 10 * 60 * 1000,  // 10 minutes
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable Cors
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
